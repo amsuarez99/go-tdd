04f2cad7023eb4319b1bf9d082bb8012b7ca8423
Adrian Suarez <adrian.suarez@qad.com>
2025-12-04 21:26:04 -0600

    structs

--- structs/structs.go
+++ structs/structs.go
@@ -1,14 +1,41 @@
 package structs
 
+import "math"
+
 type Rectangle struct {
 	width  float64
 	height float64
 }
 
-func (r *Rectangle) getPerimeter() float64 {
+type Shape interface {
+	Area() float64
+}
+
+func (r *Rectangle) Perimeter() float64 {
 	return 2 * (r.width + r.height)
 }
 
-func (r *Rectangle) getArea() float64 {
+func (r *Rectangle) Area() float64 {
 	return (r.width * r.height)
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c *Circle) Perimeter() float64 {
+	return math.Pi * c.radius
+}
+
+func (c *Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+type Triangle struct {
+	base   float64
+	height float64
+}
+
+func (c *Triangle) Area() float64 {
+	return (c.base * c.height) / 2
 }
